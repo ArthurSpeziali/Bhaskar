f4c81d1a1917f01363b2f9b3c3e1f2d325e08403
Arthur Speziali <arthur.speziali2022@gmail.com>
2024-06-21 20:45:29 -0300

    Adicionado toda a sintaxe para contas aritiméticas de soma, subtração, multiplicação, subtração e suporte a parenteses

--- lib/math.ex
+++ lib/math.ex
@@ -2,21 +2,75 @@ defmodule App.Math do
 
     @spec resolve([charlist()]) :: [charlist()]
     def resolve(equation) do
-        [
-            Integer.to_charlist(
-                resolve(:plus, equation)
-            )
-        ]
+        result = resolve_plus(equation) 
+        if is_float(result) do
+            [
+                Float.round(result, 2)
+                |>Float.to_charlist()
+            ]
+        else
+            [
+                Integer.to_charlist(result)
+            ]
+        end
     end
 
-    @spec resolve(atom(), [charlist()]) :: integer()
-    defp resolve(:plus, []), do: 0
-    defp resolve(:plus, [exp | tail]) do
+    defp resolve_plus([]), do: 0
+    defp resolve_plus([exp | tail]) do
 
         if ?. in exp do
-            List.to_float(exp) + resolve(:plus, tail)
+            List.to_float(exp) + resolve_plus(tail)
         else
-            List.to_integer(exp) + resolve(:plus, tail)
+            List.to_integer(exp) + resolve_plus(tail)
+        end
+    end
+
+
+    @spec resolve_multiply(equation :: [charlist()], char :: non_neg_integer()) :: [charlist()]
+    def resolve_multiply(equation, char) do
+        operator = Enum.at(equation, char)
+
+        previous = Enum.at(equation, char - 1)
+        previous = if ?. in previous do
+            List.to_float(previous)
+        else
+            List.to_integer(previous)
+        end
+        
+        next = Enum.at(equation, char + 1)
+        next = if ?. in next do
+            List.to_float(next)
+        else
+            List.to_integer(next)
+        end
+
+
+        result = case operator do
+            '*' ->
+                if is_float(previous * next) do
+                    Float.round(
+                        previous * next, 
+                        2
+                    )
+                else
+                    previous * next
+                end
+
+            '/' -> 
+                Float.round(
+                    previous / next,
+                    2
+                )
+        end
+
+        if is_float(result) do
+            [
+                Float.to_charlist(result)
+            ]
+        else
+            [
+                Integer.to_charlist(result)
+            ]
         end
 
     end
--- test/app_test.exs
+++ test/app_test.exs
@@ -2,7 +2,17 @@ defmodule AppTest do
 	use ExUnit.Case
 
 	@tag :plus
-    test "Some 2 + 2" do
+    test "Soma valores" do
         assert App.Main.main(["2 + 2"]) == "4"
+	end
+
+	@tag :minus
+	test "Subtrai valores" do
+		assert App.Main.main(["76.5 - 34"])
+	end
+
+	@tag :bracket
+	test "Usa parenteses" do
+		assert App.Main.main(["(56 + 8.54) - (24.3 - 4 + (-952.3))"])
 	end
 end
